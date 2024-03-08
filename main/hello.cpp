05cb33b9bec83f8d5073f51b1d2dcca9f0032ff0
Anupama Kamath <105834225+anu1712@users.noreply.github.com>
2024-03-09 00:29:02 +0530

    Added a cout line

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,12 @@
 //File: hello.cpp
+//Made a change by adding a print line
 #include <iostream>
 using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout << "Hello, CC\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
